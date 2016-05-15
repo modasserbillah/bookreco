7494f61db2526a87b60ed12812b2935b6afe260b
tishad <imtishad@gmail.com>
2016-05-15 20:42:30 +0600

    first commit with entities and their skeleton views

---
+++ rr/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rr.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^reviews/', include('reviews.urls', namespace = "reviews")),
+    url(r'^admin/', include(admin.site.urls)),
+)
